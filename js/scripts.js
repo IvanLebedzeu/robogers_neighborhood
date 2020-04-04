169aef231747b138257137f3853cab5e7b3dad82
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-04-04 15:16:01 -0700

    final version of functianallity. works as expected for all numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var digitsArray = [];
-var numArray = [];
+//var finalArray = [];
+//var numArray = [];
 var letters = /[A-Za-z]/;
+
 function convertNumbers(number) {
 
   if (number < 0 || (number.toString().match(letters))) {
@@ -9,17 +11,49 @@ function convertNumbers(number) {
   for (var i = 0; i <= number; i++){
 
    
-    digitsArray.push(i);
+    digitsArray.push(i.toString());
     console.log(digitsArray);
+
+    for (var j = 0; j < digitsArray.length; j++){
+      if (digitsArray[j].includes("3")){
+        digitsArray[j] = "Won't you be my neighbor?";
+      } else if (digitsArray[j].includes("2")){
+        digitsArray[j] = "Boop";
+      } else if (digitsArray[j].includes("1")){
+        digitsArray[j] = "Beep";
+      }
+    }
+
 
    
       
-     var index = digitsArray.indexOf(3);     
-     digitsArray[index] = "Won't you be my neighbor?";
-     var index1 = digitsArray.indexOf(2);
-     digitsArray[index1] = "Boop";
-     var index2 = digitsArray.indexOf(1);
-     digitsArray[index2] = "Beep";
+    //  var index = digitsArray.indexOf(3);     
+    //  digitsArray[index] = "Won't you be my neighbor?";
+    //  var index1 = digitsArray.indexOf(2);
+    //  digitsArray[index1] = "Boop";
+    //  var index2 = digitsArray.indexOf(1);
+    //  digitsArray[index2] = "Beep";
+    //  console.log(digitsArray);
+
+      // for(var j = 0; j< digitsArray.length; j++){
+      //   if (digitsArray[j].length() > 2 && digitsArray.indexOf(3)){
+      //     digitsArray[j] = "Won't you be my neighbor?";
+      //   }
+      // }
+
+
+
+    //  for (var index; index < digitsArray.length; index++) {
+    //    if (index = digitsArray.indexOf(3)){
+    //     digitsArray[index] = "Won't you be my neighbor?";
+    //    } else if (index = digitsArray.indexOf(2)) {
+    //     digitsArray[index] = "Boop";
+    //    } else if (index = digitsArray.indexOf(1)) {
+    //      digitsArray[index] = "Beep";
+    //    }
+    //  } return digitsArray;
+
+   
     
      
   }  return digitsArray;
@@ -42,11 +76,12 @@ $(document).ready(function(){
     });
     
 
-    var userInput = parseInt($("input#number").val());
+    var userInput = parseInt($("input#number").val()); 
     console.log(userInput);
-    var number = ("" + userInput).split("");
+    //var number = ("" + userInput).split(""); // - previos
 
-    convertNumbers(number);
+    //convertNumbers(number); // - previos
+    convertNumbers(userInput);
 
 
     $("#result").text(digitsArray);
